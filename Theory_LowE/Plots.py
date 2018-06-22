79e7d9506d9d703eb2d8df0e99b7f5f67bb2e793
marialaura1992 <33118952+marialaura1992@users.noreply.github.com>
2018-06-22 15:43:47 +0100

    cleaned up

--- Theory_LowE/Plots.py
+++ Theory_LowE/Plots.py
@@ -14,12 +14,12 @@ import ParScan
 
 SMpred = P5p_anomaly.P5p_binned() #Central value prediction
 
-FFerrBar = FFScan.FindErrBar()
-ParErrBar = ParScan.FindErrBar()
+FFerrBar = FFScan.FindErrBar() #Scan over the FF parameters uncertainties phace space
+ParErrBar = ParScan.FindErrBar() #Scan over some parameters (masses, alpha_s) phace space
 TotBarUp = []
 TotBarDown = []
 for i in range(len(FFerrBar[0])):
-    TotBarUp.append(np.sqrt(FFerrBar[0][i]**2 + ParErrBar[0][i]**2))
+    TotBarUp.append(np.sqrt(FFerrBar[0][i]**2 + ParErrBar[0][i]**2)) #Add errors in quadrature
     TotBarDown.append(np.sqrt(FFerrBar[1][i]**2 + ParErrBar[1][i]**2))
 
     
@@ -29,18 +29,7 @@ def BinPlot():
     rc('text', usetex=True)
     
     bins=[(0.1, 0.98), (1.1, 2.5), (2.5, 4.), (4., 6.0)]
-    #bins.tolist() #needed for Flavio th-prediction
-    bins=[tuple(entry) for entry in bins]
-    
-    '''
-    Max_plt = np.array(FindMaxMin()[0])
-    Max_plt = np.append(Max_plt, -1)
-    Min_plt = np.array(FindMaxMin()[1])
-    Min_plt = np.append(Min_plt, -1)
-    '''
-    
-
-    
+        
     ax=plt.gca()
     ax.set_xlim([0, 6.1])
     ax.set_ylim([-1, 1.])
@@ -48,14 +37,14 @@ def BinPlot():
         label= 'SM'
         if i>0:
             label=None
-        ax.add_patch(patches.Rectangle((bins[i][0], (SMpred[i]-TotBarDown[i])),
+        ax.add_patch(patches.Rectangle((bins[i][0], (SMpred[i]-TotBarDown[i])), #bottom left point
                                        bins[i][1]-bins[i][0],          # width
                                        (TotBarUp[i]+TotBarDown[i]),   # height
                                        ec='b', fill= True, fc= 'c',
                                        lw=True, hatch= '///',
                                        label=label, capstyle= 'butt'))
             
-            # Falvio experimental data
+    # Falvio experimental data
     measur=['LHCb B->K*mumu 2015 P 0.1-0.98',
             'LHCb B->K*mumu 2015 P 1.1-2.5',
             'LHCb B->K*mumu 2015 P 2.5-4',
@@ -65,20 +54,12 @@ def BinPlot():
                      col_dict= {'ATLAS': 'r', 'LHCb': 'k' },  #'#EB70AA' for light pink
                      divide_binwidth=False,
                      include_measurements=measur)
-    
-    # Flavio theoretical prediction
-    #fpl.bin_plot_th( '<P5p>(B0->K*mumu)', bins,
-    #                 label='SM-th-Flavio', divide_binwidth=False,
-    #                N=50,threads=2)
-    
+
     plt.xlabel('$q^2 \hspace{2pt} (GeV^2)$')
     plt.ylabel('$P5\' \hspace{2pt} (q^2)$')
     plt.legend()
     plt.title('FF+Parameter corrections added in quadrature')
-    #plt.title('$P_5\'$ prediction with $ (\delta C_7, \delta C_9, \delta C_{10}) = (.1, .1, .1)$')
     plt.show()
-    #plt.ylim(-1.2, 0.7)
-    #plt.savefig('Fig1_NewP5.png', bbox_inches='tight')
     return(0)
 
 BinPlot()
